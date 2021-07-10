ee53dcd0bde6341e2e109102ed78364762d1cc16
ItsVixano <vixanonew@gmail.com>
2021-07-10 16:33:19 +0200

    daisy: Import camera hal from LA.UM.8.6.r1-02600-89xx.0
    Change-Id: Ia56bdbdca43def4b1cec8d14f5990851d0f8059e
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -125,6 +125,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8953 \
+    libmm-qcamera \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service

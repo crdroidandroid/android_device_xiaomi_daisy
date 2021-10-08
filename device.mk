1b42fa713853c125ceebd6d962225f56f9ead40e
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 09:48:37 -0400

    daisy: Ship cgroups.json and task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for products
    launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Polaris was launched with Oreo (API level 26), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ib597263bf3b84caf2623ddc58780e1afcb985226
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -196,6 +196,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sysconfig/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # ConsumerIr
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \

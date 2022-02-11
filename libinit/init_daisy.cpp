99f83ef3444e669b31364dba6618725d07f44e89
Park Ju Hyung <qkrwngud825@gmail.com>
2022-02-11 21:06:27 +0200

    daisy: libinit: Workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    [LinkBoi00] Do not chmod SELinux nodes: e63125b43ab7461aa73784f75a5a0eff0729a2e6
    and adapt for our libinit setup
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- libinit/init_daisy.cpp
+++ libinit/init_daisy.cpp
@@ -5,9 +5,56 @@
  */
 
 #include <libinit_dalvik_heap.h>
+#include <libinit_utils.h>
 
 #include "vendor_init.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+  "ro.boot.vbmeta.device_state",
+  "ro.boot.verifiedbootstate",
+  "ro.boot.flash.locked",
+  "ro.boot.selinux",
+  "ro.boot.veritymode",
+  "ro.boot.warranty_bit",
+  "ro.warranty_bit",
+  "ro.debuggable",
+  "ro.secure",
+  "ro.build.type",
+  "ro.build.tags",
+  "ro.build.selinux",
+  NULL
+};
+
+static const char *snet_prop_value[] = {
+  "locked",
+  "green",
+  "1",
+  "enforcing",
+  "enforcing",
+  "0",
+  "0",
+  "0",
+  "1",
+  "user",
+  "release-keys",
+  "1",
+  NULL
+};
+
+static void workaround_snet_properties() {
+
+  // Hide all sensitive props
+  for (int i = 0; snet_prop_key[i]; ++i) {
+    property_override(snet_prop_key[i], snet_prop_value[i]);
+  }
+}
 
 void vendor_load_properties() {
     set_dalvik_heap();
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
 }

12b9502ae074b027616822f574ab071477743079
LinkBoi00 <linkdevel@protonmail.com>
2022-04-17 23:18:22 +0300

    daisy: init: Override APEX prop to false via libinit
    * We do not ship updatable APEXs
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- init/init_daisy.cpp
+++ init/init_daisy.cpp
@@ -104,4 +104,7 @@ void vendor_load_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
+    // APEX
+    property_override("ro.apex.updatable", "false");
 }
